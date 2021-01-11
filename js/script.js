662fd8c495043b0fcd1555e6bb905533badd2578
Vito Gentile <gentilevito1076@gmail.com>
2021-01-11 15:22:48 +0100

    Un po' di pulizia

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@
 // console.log(randomNumber);
 //La userò, all'interno di una funzione:
 function random(a, b) {
-  randomNumber = Math.floor((Math.random() * b) + a);
+  var randomNumber = Math.floor((Math.random() * b) + a);
   // console.log(randomNumber);
   return randomNumber;
 }
@@ -61,58 +61,6 @@ for ( var i = 0; i < 16; i++) {
   // }
 }
 console.log(arrayMinePosition);
-// Che può produrre numeri randomici che si ripteno; come di seguito:
-// script.js:63 (16) [100, 92, 63, 75, 18, 66, 88, 47, 11, 85, 82, 19, 82, 28, 98, 81]
-var k = 0;
-var arrayMineOk = [];
-arrayMineOk.push(arrayMinePosition[0]);
-console.log(arrayMineOk);//L'array arrayMineOk lo popolo col primo item preso dall'array precedente.
-//Il for che segue controlla solo che il primo item del nuovo array sia diverso da tutti quelli che lo precedereanno
-//Proviamo ad estendere il controllo su ciascuno degli item che seguono il primo(***)
-for (var x = 1; x < arrayMinePosition.length; x++) {
-  if (arrayMinePosition[0 + k] / arrayMinePosition[x] !== 1) {
-    // console.log(arrayMinePosition[x]);
-    arrayMineOk.push(arrayMinePosition[x]);
-    // console.log(arrayMineOk);//Aggiorna l'array ad ogni iterazione del ciclo
-  } else {
-    arrayMineOk.push(random(1,100));
-  }
-
-}
-console.log(arrayMineOk);
-//Che da un risultato del tipo:
-// script.js:63 (16) [2, 19, 65, 20, 44, 32, 62, 84, 48, 87, 2, 84, 93, 44, 88, 54]
-// script.js:69 [2]
-// script.js:76 (2) [2, 19]
-// script.js:76 (3) [2, 19, 65]
-// script.js:76 (4) [2, 19, 65, 20]
-// script.js:76 (5) [2, 19, 65, 20, 44]
-// script.js:76 (6) [2, 19, 65, 20, 44, 32]
-// script.js:76 (7) [2, 19, 65, 20, 44, 32, 62]
-// script.js:76 (8) [2, 19, 65, 20, 44, 32, 62, 84]
-// script.js:76 (9) [2, 19, 65, 20, 44, 32, 62, 84, 48]
-// script.js:76 (10) [2, 19, 65, 20, 44, 32, 62, 84, 48, 87]
-// script.js:76 (12) [2, 19, 65, 20, 44, 32, 62, 84, 48, 87, 69, 84]
-// script.js:76 (13) [2, 19, 65, 20, 44, 32, 62, 84, 48, 87, 69, 84, 93]
-// script.js:76 (14) [2, 19, 65, 20, 44, 32, 62, 84, 48, 87, 69, 84, 93, 44]
-// script.js:76 (15) [2, 19, 65, 20, 44, 32, 62, 84, 48, 87, 69, 84, 93, 44, 88]
-// script.js:76 (16) [2, 19, 65, 20, 44, 32, 62, 84, 48, 87, 69, 84, 93, 44, 88, 54]
-// script.js:82 (16) [2, 19, 65, 20, 44, 32, 62, 84, 48, 87, 69, 84, 93, 44, 88, 54]
-//oppure, similmente:
-// script.js:63 (16) [92, 14, 92, 33, 90, 65, 97, 13, 21, 66, 37, 63, 4, 85, 68, 91]
-// script.js:69 [92]
-// script.js:76 (2) [92, 14]
-// script.js:76 (4) [92, 14, 33, 33]
-// script.js:76 (5) [92, 14, 33, 33, 90]
-// script.js:76 (6) [92, 14, 33, 33, 90, 65]
-// script.js:76 (7) [92, 14, 33, 33, 90, 65, 97]
-// script.js:76 (8) [92, 14, 33, 33, 90, 65, 97, 13]
-// script.js:76 (9) [92, 14, 33, 33, 90, 65, 97, 13, 21]
-// script.js:76 (10) [92, 14, 33, 33, 90, 65, 97, 13, 21, 66]
-// script.js:76 (11) [92, 14, 33, 33, 90, 65, 97, 13, 21, 66, 37]
-// script.js:76 (12) [92, 14, 33, 33, 90, 65, 97, 13, 21, 66, 37, 63]
-// script.js:76 (13) [92, 14, 33, 33, 90, 65, 97, 13, 21, 66, 37, 63, 4]
-// script.js:76 (14) [92, 14, 33, 33, 90, 65, 97, 13, 21, 66, 37, 63, 4, 85]
-// script.js:76 (15) [92, 14, 33, 33, 90, 65, 97, 13, 21, 66, 37, 63, 4, 85, 68]
-// script.js:76 (16) [92, 14, 33, 33, 90, 65, 97, 13, 21, 66, 37, 63, 4, 85, 68, 91]
-// script.js:82 (16) [92, 14, 33, 33, 90, 65, 97, 13, 21, 66, 37, 63, 4, 85, 68, 91]
+// // Che può produrre numeri randomici che si ripteno; come di seguito:
+// // script.js:63 (16) [100, 92, 63, 75, 18, 66, 88, 47, 11, 85, 82, 19, 82, 28, 98, 81]
+// // -----------------------------------------------------------------------------------------------------------------------
